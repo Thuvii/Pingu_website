07c55cfd789ebf1a3bc64245a9fc72e042e912b9
Thuvii <buihhh321@gmail.com>
2024-07-11 22:02:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const hoverImage = document.getElementById('hoverImage');
+    const hoverSound = document.getElementById('hoverSound');
+
+    // Static image and GIF paths
+    const staticImageSrc = './access/images/aesthetic-pingu-meme-desktop-wallpaper.jpg';
+    const gifImageSrc = '/121_Noot_noot.gif';
+
+    hoverImage.addEventListener('click', () => {
+        hoverImage.src = gifImageSrc;
+        hoverSound.play().catch(error => {
+                    console.error("Sound play prevented by browser: ", error);
+                });;
+    });
+
+    hoverImage.addEventListener('mouseout', () => {
+        hoverImage.src = staticImageSrc;
+        hoverSound.pause();
+        hoverSound.currentTime = 0; // Reset the sound to the beginning
+    });
+});
